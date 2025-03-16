3ee979164f2ebf732d6d8208f87c3a733169da80
Sourav Gon <souravgon2013@gmail.com>
2025-03-17 00:12:20 +0530

    Initial Commit

---
+++ src/test/java/open/ai/chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package open.ai.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
